75929bbda2cc80676dd0de2a41698e86e949149e
lag129 <65485662+lag129@users.noreply.github.com>
2024-07-26 13:54:07 +0900

    ステップ3

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,10 @@
 fun main() {
     val buffer = readln().toCharArray()
-    val number = buffer[0] - '0'
+    var number = 0
+    var i = 0
+    while (i < buffer.size && buffer[i] in '0'..'9') {
+        number = number*10 + (buffer[i] - '0')
+        i++
+    }
     println(number)
 }
